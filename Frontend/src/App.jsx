ac61eb116c43a15d8e659434284610092f415336
vrushabh <coolvrushabh2020@gmail.com>
2025-05-26 16:36:08 +0530

    latest changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,71 +1,3 @@
-// import { useState, useEffect } from "react";
-// import "prismjs/themes/prism-tomorrow.css";
-// import Editor from "react-simple-code-editor";
-// import prism from "prismjs";
-// import "./App.css";
-// import axios from "axios";
-// import Markdown from "react-markdown";
-// import rehypeHighlight from "rehype-highlight";
-// import "highlight.js/styles/github-dark.css";
-
-
-// function App() {
-//   const [code, setCode] = useState(``);
-//   const [review, setReview] = useState("");
-
-//   useEffect(() => {
-//     prism.highlightAll();
-//   }, []);
-
-//   async function reviewCode() {
-//     const response = await axios.post("http://localhost:3000/ai/get-response", {
-//       code,
-//     });
-//     setReview(response?.data?.candidates[0]?.content?.parts[0]?.text);
-//   }
-
-//   return (
-//     <>
-//       <main>
-//         <div className="left">
-//           <div className="header">
-//             <h3>Code</h3>
-//             <p>Paste your code here and get a review</p>
-//           </div>
-//           <div className="code">
-//             <Editor
-//               value={code}
-//               onValueChange={(code) => setCode(code)}
-//               highlight={(code) =>
-//                 prism.highlight(code, prism.languages.js, "js")
-//               }
-//               padding={10}
-//               style={{
-//                 fontFamily: '"Fira code", "Fira Mono", monospace',
-//                 fontSize: 16,
-//                 height: "100%",
-//                 width: "100%",
-//               }}
-//             />
-//           </div>
-//           <div className="review" onClick={reviewCode}>
-//             Review
-//           </div>
-//         </div>
-//         <div className="right">
-//           <div className="header">
-//             <h3>Review</h3>
-//             <p>Get a review of your code</p>
-//           </div>
-//           <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
-//         </div>
-//       </main>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { useState, useEffect } from "react";
 import "prismjs/themes/prism-tomorrow.css";
 import Editor from "react-simple-code-editor";
@@ -90,9 +22,12 @@ function App() {
     setIsLoading(true);
     setReview("");
     try {
-      const response = await axios.post("http://localhost:3000/ai/get-response", {
-        code,
-      });
+      const response = await axios.post(
+        "http://localhost:3000/ai/get-response",
+        {
+          code,
+        }
+      );
       setReview(response?.data?.candidates[0]?.content?.parts[0]?.text);
     } catch (err) {
       setReview("❌ Error fetching review. Please try again.");
@@ -102,44 +37,52 @@ function App() {
 
   return (
     <>
-      <main>
-        <div className="left">
-          <div className="header">
-            <h3>Code</h3>
-            <p>Paste your code here and get a review</p>
+      <div className="page">
+        <header className="app-header">
+          <h3>Code Review</h3>
+        </header>
+        <main>
+          <div className="left">
+            <div className="header">
+              <h3>Code</h3>
+              <p>Paste your code here and get a review</p>
+            </div>
+            <div className="code">
+              <Editor
+                autoFocus={true}
+                value={code}
+                onValueChange={(code) => setCode(code)}
+                highlight={(code) =>
+                  prism.highlight(code, prism.languages.js, "js")
+                }
+                padding={10}
+                style={{
+                  fontFamily: '"Fira code", "Fira Mono", monospace',
+                  fontSize: 16,
+                  width: "100%",
+                  overflowY: "auto",
+                  width: "100%",
+                }}
+                minLength={100}
+              />
+            </div>
+            <div className="review" onClick={reviewCode}>
+              Review
+            </div>
           </div>
-          <div className="code">
-            <Editor
-              value={code}
-              onValueChange={(code) => setCode(code)}
-              highlight={(code) =>
-                prism.highlight(code, prism.languages.js, "js")
-              }
-              padding={10}
-              style={{
-                fontFamily: '"Fira code", "Fira Mono", monospace',
-                fontSize: 16,
-                height: "100%",
-                width: "100%",
-              }}
-            />
+          <div className="right">
+            <div className="header">
+              <h3>Review</h3>
+              <p>Review of your code</p>
+            </div>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+            )}
           </div>
-          <div className="review" onClick={reviewCode}>
-            Review
-          </div>
-        </div>
-        <div className="right">
-          <div className="header">
-            <h3>Review</h3>
-            <p>Get a review of your code</p>
-          </div>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
-          )}
-        </div>
-      </main>
+        </main>
+      </div>
     </>
   );
 }
